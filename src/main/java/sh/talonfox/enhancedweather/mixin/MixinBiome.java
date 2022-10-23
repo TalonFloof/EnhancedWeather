ef80b08fc0ab71d0f0cb8a8ac462c421635c68ce
TalonFox <105325988+Talon396@users.noreply.github.com>
2022-10-22 21:03:46 -0500

    Fixed Critical Bug with Snow
    Signed-off-by: TalonFox <105325988+Talon396@users.noreply.github.com>

--- src/main/java/sh/talonfox/enhancedweather/mixin/MixinBiome.java
+++ src/main/java/sh/talonfox/enhancedweather/mixin/MixinBiome.java
@@ -1,9 +1,12 @@
 package sh.talonfox.enhancedweather.mixin;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Heightmap;
+import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.biome.Biome;
@@ -18,16 +21,17 @@ public class MixinBiome {
      * @reason To allow for the creation of localized weather
      */
     @Overwrite
-    public boolean canSetSnow(WorldView worldView, BlockPos pos) {
-        if (!worldView.isSkyVisible(pos)) {
-            return false;
-        } else if (worldView.getTopPosition(Heightmap.Type.MOTION_BLOCKING, pos).getY() > pos.getY()) {
-            return false;
-        } else {
-            if(Enhancedweather.SERVER_WEATHER.getClosestCloud(new Vec3d(pos.getX(), 200, pos.getZ()), 384, true, false, false, false, 0) == null)
-                return false;
-            Biome biome = worldView.getBiome(pos).value();
-            return biome.getPrecipitation() == Biome.Precipitation.SNOW;
+    public boolean canSetSnow(WorldView world, BlockPos pos) {
+        if (!((Biome)(Object)this).doesNotSnow(pos)) {
+            if (pos.getY() >= world.getBottomY() && pos.getY() < world.getTopY() && world.getLightLevel(LightType.BLOCK, pos) < 10) {
+                BlockState blockState = world.getBlockState(pos);
+                if (blockState.isAir() && Blocks.SNOW.getDefaultState().canPlaceAt(world, pos)) {
+                    if (Enhancedweather.SERVER_WEATHER.getClosestCloud(new Vec3d(pos.getX(), 200, pos.getZ()), 384, true, false, false, false, 0) == null)
+                        return false;
+                    return true;
+                }
+            }
         }
+        return false;
     }
 }
